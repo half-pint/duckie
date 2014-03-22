c6d82a9c850d69c28e981350e9634c513613125f
Eleonora <e.ivanova.10@aberdeen.ac.uk>
2014-03-22 15:01:14 +0000

    form validation update

--- static/frmvalidation.js
+++ static/frmvalidation.js
@@ -11,31 +11,34 @@ function validateForm(){
 
 }
 
-function validateDate() {
+function validateDate(element, messageId) {
 
-    var date = document.getElementById("dateofbirth").value;
-    if (!date.match(/(^....\/..\/..$)|(^....\/..$)|(^....$)/)){
-        document.getElementById("dateofbirthMessage").innerHTML = "Wrong";
+    var date = element.value;
+    if (date.length == 0){
+        document.getElementById(messageId).innerHTML = "Right";
+        return true;}
+    else if (!date.match(/(^....\/..\/..$)|(^....\/..$)|(^....$)/)){
+        document.getElementById(messageId).innerHTML = "Wrong";
         return false;
     }
     else {
     var dateSplit = date.split("/")
     var currentTime = new Date()
 	    if (dateSplit[0]>1000 & dateSplit[0]<=currentTime.getFullYear() & !dateSplit[1]){
-            document.getElementById("dateofbirthMessage").innerHTML = "Right";
+            document.getElementById(messageId).innerHTML = "Right";
             return true;
         }
         else if (dateSplit[0]>1000 & dateSplit[0]<=currentTime.getFullYear() & dateSplit[1]>=1 & dateSplit[1]<=12 & !dateSplit[2]){
-            document.getElementById("dateofbirthMessage").innerHTML = "Right";
+            document.getElementById(messageId).innerHTML = "Right";
             return true;
         }
 
         else if (dateSplit[0]>1000 & dateSplit[0]<=currentTime.getFullYear() & dateSplit[1]>=1 & dateSplit[1]<=12 &  dateSplit[2]>=1 & dateSplit[2]<=31){
-            document.getElementById("dateofbirthMessage").innerHTML = "Right";
+            document.getElementById(messageId).innerHTML = "Right";
             return true;
         }
         else{
-        document.getElementById("dateofbirthMessage").innerHTML = "Wrong";
+        document.getElementById(messageId).innerHTML = "Wrong";
         return false;
         }
    
@@ -44,21 +47,21 @@ function validateDate() {
 
 }
 
-
-function validateDateRange(){
-if (!isItAYear("dateofbirthRangeFrom") & !isItAYear("dateofbirthRangeTo")){
-document.getElementById("dateofbirthMessageRange").innerHTML = "Please enter years in the format yyyy";
+function validateDateRange(idF,idT,idMessage){
+if (!isItAYear(idF) | !isItAYear(idT)){
+document.getElementById(idMessage).innerHTML = "Please enter years in the format yyyy";
 return false;
 }
-else if (document.getElementById("dateofbirthRangeTo").value < document.getElementById("dateofbirthRangeFrom").value)
-{document.getElementById("dateofbirthMessageRange").innerHTML = "The year 'from' should be before the year 'to' ";
+else if (document.getElementById(idF).value > document.getElementById(idT).value)
+{document.getElementById(idMessage).innerHTML = "The year 'from' should be before the year 'to' ";
 return false;
 }
-else {document.getElementById("dateofbirthMessageRange").innerHTML = "Yaaay";
+else {document.getElementById(idMessage).innerHTML = "Yaaay";
 return true;
 
 }
 }
+
 
 function isItAYear(id){
 var year = document.getElementById(id).value;
@@ -73,12 +76,29 @@ var currentTime = new Date()
 function tgl(element){
 if (element.value == 'y'){
 document.getElementById("dateofbirth").disabled = true;
-document.getElementById("dateofbirthRangeFrom").disabled = false;
-document.getElementById("dateofbirthRangeTo").disabled = false;
+document.getElementById("dobRangeFrom").disabled = false;
+document.getElementById("dobRangeTo").disabled = false;
 }
 else{
 document.getElementById("dateofbirth").disabled = false;
-document.getElementById("dateofbirthRangeFrom").disabled = true;
-document.getElementById("dateofbirthRangeTo").disabled = true;
+document.getElementById("dobRangeFrom").disabled = true;
+document.getElementById("dobRangeTo").disabled = true;
 }
+}
+
+function tglD(element){
+if (element.value == 'y'){
+document.getElementById("dateofdeath").disabled = true;
+document.getElementById("dodRangeFrom").disabled = false;
+document.getElementById("dodRangeTo").disabled = false;
+}
+else{
+document.getElementById("dateofdeath").disabled = false;
+document.getElementById("dodRangeFrom").disabled = true;
+document.getElementById("dodRangeTo").disabled = true;
+}
+}
+
+function messagePrompt(id,text){
+document.getElementById(id).innerHTML = text;
 }
